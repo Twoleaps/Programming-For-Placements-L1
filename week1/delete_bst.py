a471491cb7dcb7bff152584f3c1b8ea47912b455
Ashutosh Singh <ashutosh2564@gmail.com>
2017-12-26 13:13:21 +0530

    update delete_bst.py
    removed nested if

--- week1/delete_bst.py
+++ week1/delete_bst.py
@@ -31,18 +31,8 @@ def deleteNode(root, key):
     return root 
     if key < root.key:
         root.left = deleteNode(root.left, key)
-    elif(key > root.key):
+    else(key > root.key):
         root.right = deleteNode(root.right, key)
-    else:
-        if root.left is None :
-            temp = root.right 
-            root = None
-            return temp 
-             
-        elif root.right is None :
-            temp = root.left 
-            root = None
-            return temp
  
         temp = minValueNode(root.right)
         root.key = temp.key
